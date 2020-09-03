3476b95883f7c86aef1e7765857978b85b677a8e
shimaayasser66 <s.yasser@studio50s.com>
2020-09-03 22:16:50 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/flutter/flutter_movie_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.flutter_movie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/strings/strings.dart
@@ -0,0 +1,31 @@
+class Strings{
+
+  final String appTitle = 'Movie_App';
+
+  final String apiKey = "8a1227b5735a7322c4a43a461953d4ff";
+
+  final String mainUrl = "https://api.themoviedb.org/3";
+
+  final String no_more_movies="No More Movies";
+
+  final String no_more_persons="No More Persons";
+
+  final String casts = "CASTS";
+
+  final String cast_image_baseUrl="https://image.tmdb.org/t/p/w300/";
+
+  final String genre_image_baseUrl="https://image.tmdb.org/t/p/w200/";
+
+  final String person_image_baseUrl="https://image.tmdb.org/t/p/w300/";
+
+  final String similarmovie_image_baseUrl="https://image.tmdb.org/t/p/w300/";
+
+  final String overView = "OVERVIEW";
+
+  final String genres = "GENRES";
+
+  final String trending_persons_on_this_week ="TRENDING PERSONS ON THIS WEEK";
+
+
+
+}
